4d6f3f04555fdf2e0f0db6eff9d85333316f58d2
Justin Bushfield <justinbushfield@gmail.com>
2026-03-03 15:30:53 -0500

    Initial Commit

---
+++ PicturePicker/PicturePickerApp.swift
@@ -0,0 +1,17 @@
+//
+//  PicturePickerApp.swift
+//  PicturePicker
+//
+//  Created by Justin Bushfield on 2026-03-03.
+//
+
+import SwiftUI
+
+@main
+struct PicturePickerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
